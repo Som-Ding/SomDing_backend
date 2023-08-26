4724740e7ce5c577d5d63e7df949d5dfcef0e3b8
wjdek <wjdekdus62@naver.com>
2023-08-26 19:57:48 +0900

    1. 초기 세팅 관련

---
+++ src/main/java/com/example/somding_backend/SomDingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.somding_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomDingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomDingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/somding_backend/SomDingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.somding_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomDingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
